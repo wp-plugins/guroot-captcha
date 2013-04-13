b2d0502cca69c8691f55943bf94fc10f375f522a
Jonathan Fleury <Jonathan Fleury@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-13 21:28:32 +0000

    Update 0.2.2
    git-svn-id: https://plugins.svn.wordpress.org/guroot-captcha/trunk@697086 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- guroot_captcha.php
+++ guroot_captcha.php
@@ -4,7 +4,7 @@
   Plugin Name: Guroot Captcha
   Plugin URI:
   Description: Simple Captcha 
-  Version: 0.2.1
+  Version: 0.2.2
   Author: Jonathan Fleury
   Author URI: http://www.guroot.com
   License: GPL2
